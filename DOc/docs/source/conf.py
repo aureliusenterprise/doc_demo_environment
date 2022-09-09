12a6f91c180c73c68a56ca9ead062dee2248b9fb
Américo <americo.chiclana@aureliusenterprise.com>
2022-09-09 13:49:06 +0200

    change

--- DOc/docs/source/conf.py
+++ DOc/docs/source/conf.py
@@ -17,8 +17,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-'sphinx_rtd_theme']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -28,7 +27,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 #alabaster is the other theme
-html_theme = 'sphinx_rtd_theme'
+html_theme = ''
 html_static_path = ['_static']
 html_logo = '_static/logo/logo-big.png'
 html_favicon = '_static/favicon/fav.png'
