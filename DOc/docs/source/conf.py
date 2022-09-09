b664d9b1e83dc09a6c664b9666ab913a7686cf4b
Américo <americo.chiclana@aureliusenterprise.com>
2022-09-09 08:20:03 +0200

    root

--- DOc/docs/source/conf.py
+++ DOc/docs/source/conf.py
@@ -1,5 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
 #
+
+root_doc = 'contents'
+
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
