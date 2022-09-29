1c7dc3a4640cfaf0a79674a8405a3d34b9fc417d
Américo <113525921+AmericoChiclana@users.noreply.github.com>
2022-09-29 13:51:06 +0200

    last commit to main

--- DOc/docs/source/conf.py
+++ DOc/docs/source/conf.py
@@ -30,5 +30,5 @@ exclude_patterns = []
 #alabaster is the other theme
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
-html_logo = '_static/logo/logo1.png'
+html_logo = '_static/logo/logo-white.png'
 html_favicon = '_static/favicon/fav.png'
